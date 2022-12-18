be99d11f0dce9fc8a2a60debc78fef71d5ba31fe
Nihar11789 <piperace11789@gmail.com>
2022-12-18 10:42:48 +0530

    workdone day-5

---
+++ flight.js
@@ -0,0 +1,25 @@
+// owl-carousal------------------------------------------------------------------------
+
+let count = 0;
+document.querySelector("#ltexarroww").onclick = () => {
+  console.log("clicked");
+
+  if (count == 0) {
+    return;
+  }
+  count--;
+  let val = count * 100;
+  document.querySelector("#explore_containerr").style.transform = `translateX(-${val}px)`;
+};
+
+document.querySelector("#rtexarroww").onclick = () => {
+  if (count > 4) {
+    return;
+  }
+  count++;
+  let val = count * 100;
+  document.querySelector("#explore_containerr").style.transform = `translateX(-${val}px)`;
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
